9c46f5d49e7c45db0ba86d519d3a32d7848b9e09
Dasun Tharanga <hello@dasun.dev>
2024-04-08 14:04:25 +0530

    feat: add service provider

---
+++ src/PayHereServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dasundev\PayHere;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class PayHereServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('payhere');
+    }
+}
