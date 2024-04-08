88b34c40d613b79935130dab39a07af9395455b9
Dasun Tharanga <hello@dasun.dev>
2024-04-08 15:03:19 +0530

    feat: add merchant_id to config

--- config/payhere.php
+++ config/payhere.php
@@ -7,13 +7,25 @@ return [
     | PayHere API Base URL
     |--------------------------------------------------------------------------
     |
-    | Here you can set the base URL for the PayHere API. This URL is important
-    | for connecting to the PayHere API. By default, it's set to the sandbox URL.
-    | Make sure to switch to the live URL when your application goes live.
+    | This API Base URL is important for connecting to the PayHere API. By default,
+    | it's set to the sandbox URL. Make sure to switch to the live URL when your
+    | application goes live.
     |
     | Live URL:    https://www.payhere.lk
     | Sandbox URL: https://sandbox.payhere.lk
     */
 
     'base_url' => env('PAYHERE_BASE_URL', 'https://sandbox.payhere.lk'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | PayHere Merchant ID
+    |--------------------------------------------------------------------------
+    |
+    | Your merchant ID is important for identifying your merchant account
+    | when connecting to the PayHere API. You can find your merchant ID
+    | at https://www.payhere.lk/merchant/domains.
+    */
+
+    'merchant_id' => env('PAYHERE_MERCHANT_ID'),
 ];
