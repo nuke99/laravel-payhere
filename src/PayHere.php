934cd4bf5b870e6eefd9ffcb544f47a2ac2595fd
Dasun Tharanga <hello@dasun.dev>
2024-04-09 20:55:38 +0530

    feat: add use customer model method

--- src/PayHere.php
+++ src/PayHere.php
@@ -4,5 +4,21 @@ namespace Dasundev\PayHere;
 
 class PayHere
 {
+    /**
+     * The default customer model class name.
+     *
+     * @var string
+     */
     public static string $customerModel = 'App\\Models\\User';
+
+    /**
+     * Set the customer model class name.
+     * 
+     * @param $customerModel
+     * @return void
+     */
+    public static function useCustomerModel($customerModel): void
+    {
+        static::$customerModel = $customerModel;
+    }
 }
