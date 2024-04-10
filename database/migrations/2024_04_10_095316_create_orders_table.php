4c3cb64e7258a0632d07614ef624c397eda603db
Dasun Tharanga <hello@dasun.dev>
2024-04-10 10:32:39 +0530

    feat: create orders table migration

---
+++ database/migrations/2024_04_10_095316_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Dasundev\PayHere\PayHere;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(PayHere::$customerModel)->constrained();
+            $table->enum('status', [
+                'on_hold',
+                'pending',
+                'processing',
+                'shipped',
+                'delivered',
+                'completed',
+                'refunded',
+                'cancelled',
+            ]);
+            $table->float('total');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
