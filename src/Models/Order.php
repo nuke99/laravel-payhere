025600c2d72acaa76764f9a0e9d6e1afd2d79d89
Dasun Tharanga <hello@dasun.dev>
2024-04-10 16:27:00 +0530

    feat: update phpdoc comment

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property mixed $user
+ */
 class Order extends Model
 {
     protected $guarded = [];
