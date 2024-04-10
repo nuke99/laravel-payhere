d74446ea986b4ae4244cf25216f30889f50b73e6
Dasun Tharanga <hello@dasun.dev>
2024-04-10 10:18:39 +0530

    feat: create order model

---
+++ src/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dasundev\PayHere\Models;
+
+use Dasundev\PayHere\PayHere;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(PayHere::$customerModel);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
