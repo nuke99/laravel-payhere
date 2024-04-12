df700ea91dc1ea116ff16aa4f3e9746e5de0dc24
Dasun Tharanga <hello@dasun.dev>
2024-04-12 11:48:13 +0530

    feat: add generate hash concern

---
+++ src/Concerns/GenerateHash.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dasundev\PayHere\Concerns;
+
+trait GenerateHash
+{
+    /**
+     * Generate a hash string.
+     *
+     * The hash value is required starting from 2023-01-16.
+     *
+     * @return string
+     */
+    public function generateHash(): string
+    {
+        return strtoupper(
+            md5(
+                config('payhere.merchant_id') .
+                $this->order->id .
+                number_format($this->order->total, 2, '.', '') .
+                config('payhere.currency') .
+                strtoupper(md5(config('payhere.merchant_secret')))
+            )
+        );
+    }
+}
