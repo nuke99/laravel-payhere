cb74ee9aa7024374f1ad04c243b7b3f03aabc41c
Dasun Tharanga <hello@dasun.dev>
2024-04-12 12:42:35 +0530

    feat: update payhere webhook route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use Dasundev\PayHere\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('payhere/webhook', [WebhookController::class, 'handleWebhook'])->name('payhere.webhook');
+Route::get('payhere/webhook', [WebhookController::class, 'handleWebhook'])
+    ->middleware('signed')
+    ->name('payhere.webhook');
