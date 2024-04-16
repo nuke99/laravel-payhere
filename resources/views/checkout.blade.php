7fe3e215492479f0d5e03985df9e05056f2966fd
Dasun Tharanga <hello@dasun.dev>
2024-04-16 21:56:16 +0530

    feat: update checkout blade

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -11,23 +11,26 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </head>
 <body>
-<form action="{{ $form['action'] }}" method="post">
-    <input type="hidden" name="merchant_id" value="{{ $form['merchant_id'] }}">
-    <input type="hidden" name="return_url" value="{{ $form['return_url'] }}">
-    <input type="hidden" name="cancel_url" value="{{ $form['cancel_url'] }}">
-    <input type="hidden" name="notify_url" value="{{ $form['notify_url'] }}">
-    <input type="hidden" name="order_id" value="{{ $form['order_id'] }}">
-    <input type="hidden" name="items" value="{{ $form['items'] }}">
-    <input type="hidden" name="currency" value="{{ $form['currency'] }}">
-    <input type="hidden" name="amount" value="{{ $form['amount'] }}">
-    <input type="hidden" name="first_name" value="{{ $form['first_name'] }}">
-    <input type="hidden" name="last_name" value="{{ $form['last_name'] }}">
-    <input type="hidden" name="email" value="{{ $form['email'] }}">
-    <input type="hidden" name="phone" value="{{ $form['phone'] }}">
-    <input type="hidden" name="address" value="{{ $form['address'] }}">
-    <input type="hidden" name="city" value="{{ $form['city'] }}">
-    <input type="hidden" name="country" value="{{ $form['country'] }}">
-    <input type="hidden" name="hash" value="{{ $form['hash'] }}">
+<form action="{{ $form['other']['action'] }}" method="post">
+    <input type="hidden" name="merchant_id" value="{{ $form['other']['merchant_id'] }}">
+    <input type="hidden" name="return_url" value="{{ $form['other']['return_url'] }}">
+    <input type="hidden" name="cancel_url" value="{{ $form['other']['cancel_url'] }}">
+    <input type="hidden" name="notify_url" value="{{ $form['other']['notify_url'] }}">
+    <input type="hidden" name="order_id" value="{{ $form['other']['order_id'] }}">
+    <input type="hidden" name="items" value="{{ $form['other']['items'] }}">
+    <input type="hidden" name="currency" value="{{ $form['other']['currency'] }}">
+    <input type="hidden" name="amount" value="{{ $form['other']['amount'] }}">
+    <input type="hidden" name="first_name" value="{{ $form['customer']['first_name'] }}">
+    <input type="hidden" name="last_name" value="{{ $form['customer']['last_name'] }}">
+    <input type="hidden" name="email" value="{{ $form['customer']['email'] }}">
+    <input type="hidden" name="phone" value="{{ $form['customer']['phone'] }}">
+    <input type="hidden" name="address" value="{{ $form['customer']['address'] }}">
+    <input type="hidden" name="city" value="{{ $form['customer']['city'] }}">
+    <input type="hidden" name="country" value="{{ $form['customer']['country'] }}">
+    <input type="hidden" name="hash" value="{{ $form['other']['hash'] }}">
+    @foreach($form['items'] as $key => $value)
+        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+    @endforeach
     <input type="submit" value="Checkout">
 </form>
 </body>
