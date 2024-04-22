c1d3583b3e2d501e09c0c4d38b4de40f52a01701
Dasun Tharanga <hello@dasun.dev>
2024-04-22 17:02:59 +0530

    fix: webhook

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Dasundev\PayHere\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('payhere/webhook', [WebhookController::class, 'handleWebhook'])
+Route::post('payhere/webhook', [WebhookController::class, 'handleWebhook'])
     ->middleware('signed')
     ->name('payhere.webhook');
