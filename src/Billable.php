cd0339105ecadaa18869aad971de6213d2eeb226
Dasun Tharanga <hello@dasun.dev>
2024-04-22 16:55:49 +0530

    feat: add relationships

--- src/Billable.php
+++ src/Billable.php
@@ -4,9 +4,24 @@ namespace Dasundev\PayHere;
 
 use Dasundev\PayHere\Concerns\GenerateHash;
 use Dasundev\PayHere\Concerns\HandleCheckout;
+use Dasundev\PayHere\Models\Subscription;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
+/**
+ * @method morphMany(string $customerModel, string $name)
+ */
 trait Billable
 {
     use HandleCheckout;
     use GenerateHash;
+
+    public function payments(): MorphMany
+    {
+        return $this->morphMany(PayHere::$customerModel, 'billable');
+    }
+
+    public function subscriptions(): MorphMany
+    {
+        return $this->morphMany(Subscription::class, 'billable');
+    }
 }
