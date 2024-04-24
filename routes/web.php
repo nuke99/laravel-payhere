07f418a036db9a1d0acc16bf7063e24f0facbb5e
Dasun Tharanga <hello@dasun.dev>
2024-04-24 19:47:54 +0530

    feat: remove signed middleware from payhere success route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Dasundev\PayHere\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'payhere', 'as' => 'payhere.'], function () {
-    Route::view('/success', 'payhere::success')->middleware('signed')->name('success');
+    // Skipped using the "signed" middleware
+    // because the controller needs to manually ignore the "order_id" parameter
+    // that comes with the Payhere return URL.
+    Route::view('/success', 'payhere::success')->name('success');
     Route::post('/webhook', [WebhookController::class, 'handleWebhook'])->middleware('signed')->name('webhook');
 });
