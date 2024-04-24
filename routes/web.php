4d394ad4102547ebf7c79aa89ae02a1382a626c4
Dasun Tharanga <hello@dasun.dev>
2024-04-24 19:48:44 +0530

    refactor: return url

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ Route::group(['prefix' => 'payhere', 'as' => 'payhere.'], function () {
     // Skipped using the "signed" middleware
     // because the controller needs to manually ignore the "order_id" parameter
     // that comes with the Payhere return URL.
-    Route::view('/success', 'payhere::success')->name('success');
+    Route::view('/return', 'payhere::return')->name('return');
     Route::post('/webhook', [WebhookController::class, 'handleWebhook'])->middleware('signed')->name('webhook');
 });
