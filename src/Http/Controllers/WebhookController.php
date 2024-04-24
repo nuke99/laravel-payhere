050c731bac8913f6cbf3b8fe335245894c77560a
Dasun Tharanga <hello@dasun.dev>
2024-04-24 23:57:03 +0530

    feat: update handle webhook

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -2,6 +2,7 @@
 
 namespace Dasundev\PayHere\Http\Controllers;
 
+use Dasundev\PayHere\Http\Requests\WebhookRequest;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -10,8 +11,12 @@ class WebhookController extends Controller
     /**
      * Handle incoming webhook notification from PayHere.
      */
-    public function handleWebhook(Request $request): void
+    public function handleWebhook(WebhookRequest $request): void
     {
-        //
+        // TODO: Create the payment
+
+        if ($request->isRecurring()) {
+            // TODO: Create the subscription
+        }
     }
 }
