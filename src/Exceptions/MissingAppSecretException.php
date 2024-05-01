b5b7755c2c9a3f693031714c5569a6785717b193
Dasun Tharanga <hello@dasun.dev>
2024-05-01 22:21:42 +0530

    feat: add missing app secret exception

---
+++ src/Exceptions/MissingAppSecretException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dasundev\PayHere\Exceptions;
+
+use Exception;
+
+class MissingAppSecretException extends Exception
+{
+    protected $message = 'The PAYHERE_APP_SECRET is missing. Please add it to your .env file.';
+}
