9ffb0a6259dc59cb00fc706bd6fb56085760cfaf
Dasun Tharanga <hello@dasun.dev>
2024-05-01 22:23:55 +0530

    feat: add default auth

--- src/Http/Integrations/PayHere/PayHereConnector.php
+++ src/Http/Integrations/PayHere/PayHereConnector.php
@@ -2,12 +2,32 @@
 
 namespace Dasundev\PayHere\Http\Integrations\PayHere;
 
+use Dasundev\PayHere\Exceptions\MissingAppIdException;
+use Dasundev\PayHere\Exceptions\MissingAppSecretException;
+use Dasundev\PayHere\PayHere;
+use Saloon\Http\Auth\TokenAuthenticator;
 use Saloon\Http\Connector;
 
 class PayHereConnector extends Connector
 {
+    private string $token;
+
+    /**
+     * @throws MissingAppIdException
+     * @throws MissingAppSecretException
+     */
+    public function __construct()
+    {
+        $this->token = PayHere::generateAuthorizeCode();
+    }
+
     public function resolveBaseUrl(): string
     {
         return config('payhere.base_url');
+    }
+
+    protected function defaultAuth(): TokenAuthenticator
+    {
+        return new TokenAuthenticator($this->token);
     }
 }
