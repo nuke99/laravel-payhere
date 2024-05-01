f664fc422204776f05432a204926ad2a056d1e0c
Dasun Tharanga <hello@dasun.dev>
2024-05-01 22:53:57 +0530

    feat: add defaultOauthConfig method

--- src/Http/Integrations/PayHere/PayHereConnector.php
+++ src/Http/Integrations/PayHere/PayHereConnector.php
@@ -5,11 +5,15 @@ namespace Dasundev\PayHere\Http\Integrations\PayHere;
 use Dasundev\PayHere\Exceptions\MissingAppIdException;
 use Dasundev\PayHere\Exceptions\MissingAppSecretException;
 use Dasundev\PayHere\PayHere;
+use Saloon\Helpers\OAuth2\OAuthConfig;
 use Saloon\Http\Auth\TokenAuthenticator;
 use Saloon\Http\Connector;
+use Saloon\Traits\OAuth2\ClientCredentialsGrant;
 
 class PayHereConnector extends Connector
 {
+    use ClientCredentialsGrant;
+
     private string $token;
 
     /**
@@ -24,6 +28,14 @@ class PayHereConnector extends Connector
     public function resolveBaseUrl(): string
     {
         return config('payhere.base_url');
+    }
+
+    protected function defaultOauthConfig()
+    {
+        return OAuthConfig::make()
+            ->setClientId(config('payhere.app_id'))
+            ->setClientSecret(config('payhere.app_secret'))
+            ->setTokenEndpoint('/merchant/v1/oauth/token');
     }
 
     protected function defaultAuth(): TokenAuthenticator
