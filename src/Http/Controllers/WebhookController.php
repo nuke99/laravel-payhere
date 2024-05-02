81ca78665a2c9ab878843491dddbf4cc6b9aadcc
Dasun Tharanga <hello@dasun.dev>
2024-05-02 20:51:57 +0530

    feat: update webhook controller

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -27,30 +27,49 @@ class WebhookController extends Controller
 
         if (! $verified) {
             return;
-        } else {
-            $order = PayHere::$orderModel::find($orderId);
+        }
 
-            if (! $order) {
-                return;
-            }
+        $order = PayHere::$orderModel::find($orderId);
 
-            $relationship = PayHere::$customerRelationship;
+        if (! $order) {
+            return;
+        }
 
-            $user = $order->{$relationship};
+        $relationship = PayHere::$customerRelationship;
 
-            Payment::create([
+        $user = $order->{$relationship};
+
+        Payment::create([
+            'billable_id' => $user->id,
+            'billable_type' => PayHere::$customerModel,
+            'order_id' => $request->order_id,
+            'payment_id' => $request->payment_id,
+            'subscription_id' => $request->subscription_id,
+            'payhere_amount' => $request->payhere_amount,
+            'payhere_currency' => $request->payhere_currency,
+            'status_code' => $request->status_code,
+            'md5sig' => $request->md5sig,
+            'method' => $request->input('method'),
+            'card_holder_name' => $request->card_holder_name,
+            'card_no' => $request->card_no,
+            'card_expiry' => $request->card_expiry,
+            'message_type' => $request->message_type,
+            'item_recurrence' => $request->item_recurrence,
+            'item_duration' => $request->item_duration,
+            'item_rec_status' => $request->item_rec_status,
+            'item_rec_date_next' => $request->item_rec_date_next,
+            'item_rec_install_paid' => $request->item_rec_install_paid,
+            'customer_token' => $request->customer_token,
+            'custom_1' => $request->custom_1,
+            'custom_2' => $request->custom_2,
+        ]);
+
+        if ($request->isRecurring()) {
+            Subscription::create([
                 'billable_id' => $user->id,
                 'billable_type' => PayHere::$customerModel,
-                $request->all(),
+                'ends_at' => $request->item_duration,
             ]);
-
-            if ($request->isRecurring()) {
-                Subscription::create([
-                    'billable_id' => $user->id,
-                    'billable_type' => PayHere::$customerModel,
-                    'ends_at' => $request->item_duration,
-                ]);
-            }
         }
     }
 }
