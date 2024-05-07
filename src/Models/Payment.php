f850cb60cc6a2993d826aea888216ff146dd79b7
Dasun Tharanga <hello@dasun.dev>
2024-05-07 19:38:06 +0530

    feat: add $casts property

--- src/Models/Payment.php
+++ src/Models/Payment.php
@@ -2,9 +2,18 @@
 
 namespace Dasundev\PayHere\Models;
 
+use Dasundev\PayHere\Enums\PaymentMethod;
+use Dasundev\PayHere\Enums\PaymentStatus;
+use Dasundev\PayHere\Enums\RecurringStatus;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
     protected $guarded = [];
+
+    protected $casts = [
+        'method' => PaymentMethod::class,
+        'status_code' => PaymentStatus::class,
+        'message_type' => RecurringStatus::class
+    ];
 }
