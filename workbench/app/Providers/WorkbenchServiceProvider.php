f0af925c31cd1d9bc3f2317db2594c91d2e9af58
Dasun Tharanga <hello@dasun.dev>
2024-05-19 20:15:53 +0530

    feat: configure workbench

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -5,11 +5,13 @@ namespace Workbench\App\Providers;
 use Dasundev\PayHere\PayHere;
 use Illuminate\Support\ServiceProvider;
 use Workbench\App\Models\Order;
+use Workbench\App\Models\User;
 
 class WorkbenchServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        PayHere::useCustomerModel(User::class);
         PayHere::useOrderModel(Order::class);
     }
 }
