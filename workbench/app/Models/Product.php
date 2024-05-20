6c10a001bf121c9fc52df1e248ad27108a3dc4fc
Dasun Tharanga <hello@dasun.dev>
2024-05-20 13:22:29 +0530

    test: product model has factory

--- workbench/app/Models/Product.php
+++ workbench/app/Models/Product.php
@@ -2,11 +2,14 @@
 
 namespace Workbench\App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Product extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     public function lines(): MorphMany
