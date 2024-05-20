f2ea126dfad8ae2190e590e245b2b70dd5930b48
Dasun Tharanga <hello@dasun.dev>
2024-05-20 21:00:52 +0530

    chore: clean code

--- tests/Browser/CheckoutTest.php
+++ tests/Browser/CheckoutTest.php
@@ -6,17 +6,16 @@ use Dasundev\PayHere\Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Workbench\App\Models\User;
 
-class CheckoutTest extends DuskTestCase {
-
+class CheckoutTest extends DuskTestCase
+{
     public function test_render_checkout_page()
     {
         $user = User::factory()->create();
 
         $this->browse(function (Browser $browser) use ($user) {
             $browser->loginAs($user)
-                ->visit('/checkout')
-                ->assertUrlIs('/checkout');
+                ->visit('/welcome')
+                ->assertUrlIs('/welcome');
         });
     }
-
 }
