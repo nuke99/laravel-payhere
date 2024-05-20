4f1662da03b42dbf59000a89cfec60d37eac4b2b
Dasun Tharanga <hello@dasun.dev>
2024-05-20 14:26:50 +0530

    test: it can render checkout page

---
+++ tests/Feature/Http/Controllers/CheckoutTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use Workbench\App\Models\User;
+
+it('it can render checkout page', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->get('/checkout');
+
+    $response
+        ->assertStatus(200)
+        ->assertSeeText('Redirecting...');
+});
