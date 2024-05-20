a8f60e1317c2bb23a6c8f4169a4ee77d199db31c
Dasun Tharanga <hello@dasun.dev>
2024-05-20 13:16:01 +0530

    test: create product factory

---
+++ workbench/database/factories/ProductFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Workbench\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Workbench\App\Models\Product;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title,
+            'price' => fake()->numberBetween(100, 1000)
+        ];
+    }
+}
