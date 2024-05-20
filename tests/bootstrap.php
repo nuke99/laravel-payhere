73b45bf31e747eb051b32d726fa1fa66a8ac7a7d
Dasun Tharanga <hello@dasun.dev>
2024-05-20 19:17:34 +0530

    test: update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,25 @@
 <?php
 
-use Orchestra\Testbench\Dusk\Options;
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here, so we don't need to manually load our classes.
+|
+*/
 
-Options::withoutUI();
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Enable Headless Mode
+|--------------------------------------------------------------------------
+|
+| This setting enables headless mode for Testbench Dusk, allowing browser
+| tests to run without a graphical user interface.
+|
+*/
+\Orchestra\Testbench\Dusk\Options::withoutUI();
