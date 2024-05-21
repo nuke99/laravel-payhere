c818d0f6604813a954f5f2237173c985bc5b496f
Dasun Tharanga <hello@dasun.dev>
2024-05-21 13:27:26 +0530

    test: update tests

--- tests/Browser/CheckoutTest.php
+++ tests/Browser/CheckoutTest.php
@@ -3,6 +3,7 @@
 namespace Dasundev\PayHere\Tests\Browser;
 
 use Dasundev\PayHere\Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Dusk\Browser;
 use PHPUnit\Framework\Attributes\Test;
@@ -10,6 +11,8 @@ use Workbench\App\Models\User;
 
 class CheckoutTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     #[Test]
     public function it_can_render_checkout_page()
     {
