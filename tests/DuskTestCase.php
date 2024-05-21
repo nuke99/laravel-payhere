de9b69ebb4b81b29f3fb23641ae1732be078512c
Dasun Tharanga <hello@dasun.dev>
2024-05-21 08:05:57 +0530

    test: clean code

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -4,8 +4,9 @@ namespace Dasundev\PayHere\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\Dusk\TestCase;
 
-abstract class DuskTestCase extends \Orchestra\Testbench\Dusk\TestCase
+abstract class DuskTestCase extends TestCase
 {
     use WithWorkbench;
     use RefreshDatabase;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,9 @@ namespace Dasundev\PayHere\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends BaseTestCase
 {
     use WithWorkbench;
     use RefreshDatabase;
