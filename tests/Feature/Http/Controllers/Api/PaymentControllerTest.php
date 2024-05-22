54b7a187d9c3c82895959620101dbcfb575b0dae
Dasun Tharanga <hello@dasun.dev>
2024-05-22 20:27:07 +0530

    test: it can search payments with an order id

--- tests/Feature/Http/Controllers/Api/PaymentControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PaymentControllerTest.php
@@ -14,3 +14,20 @@ it('it can search payments without an order id', function () {
             'status' => 1
         ]);
 });
+
+it('it can search payments with an order id', function () {
+    $response = $this->getJson('payhere/api/payments/search?order_id=1');
+
+    $response
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'status',
+            'msg',
+            'data'
+        ])
+        ->assertJsonFragment([
+            'status' => 1,
+            'msg' => 'Payments with order_id:1',
+            'description' => 'Order #1',
+        ]);
+});
