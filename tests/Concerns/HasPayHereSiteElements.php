3de25ce520ed2d9869d28d4bd6213cccdff96438
Dasun Tharanga <hello@dasun.dev>
2024-05-22 22:28:47 +0530

    test: create payhere site elements concern

---
+++ tests/Concerns/HasPayHereSiteElements.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dasundev\PayHere\Tests\Concerns;
+
+trait HasPayHereSiteElements
+{
+    public static function siteElements(): array
+    {
+        return [
+            '@visa' => 'div#payment_container_VISA',
+            '@payment-frame' => 'iframe#pg_iframe',
+            '@card-holder-name' => "input[name='cardHolderName']",
+            '@card-no' => "input[name='cardNo']",
+            '@card-secure-id' => "input[name='cardSecureId']",
+            '@card-expiry' => "input[name='cardExpiry']",
+            '@pay' => "button[type='submit'].btn-primary",
+        ];
+    }
+}
