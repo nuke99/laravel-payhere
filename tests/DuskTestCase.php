7273214ca56306a966a99ad6c3281ea3ccc2556a
Dasun Tharanga <hello@dasun.dev>
2024-05-22 07:22:01 +0530

    test: dust test case

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,10 +2,24 @@
 
 namespace Dasundev\PayHere\Tests;
 
+use Dasundev\PayHere\PayHereServiceProvider;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\Dusk\TestCase;
+use Workbench\App\Models\User;
 
 abstract class DuskTestCase extends TestCase
 {
     use WithWorkbench;
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('auth.providers.users.model', User::class);
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            PayHereServiceProvider::class
+        ];
+    }
 }
