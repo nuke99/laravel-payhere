63b6d448ce83cb702edff72530c0cd5691d001f4
Dasun Tharanga <hello@dasun.dev>
2024-05-22 08:44:29 +0530

    test: re-add checkout test

---
+++ tests/Browser/CheckoutTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dasundev\PayHere\Tests\Browser;
+
+use Dasundev\PayHere\Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use PHPUnit\Framework\Attributes\Test;
+use Workbench\App\Models\User;
+
+class CheckoutTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    #[Test]
+    public function it_can_render_checkout_page()
+    {
+        $user = User::factory()->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->assertAuthenticatedAs($user);
+        });
+    }
+}
