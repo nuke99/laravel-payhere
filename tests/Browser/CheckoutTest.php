1fb6cabb5583905b91c19016a4a97c4498fb6a55
Dasun Tharanga <hello@dasun.dev>
2024-05-22 18:33:30 +0530

    test: improve test case

--- tests/Browser/CheckoutTest.php
+++ tests/Browser/CheckoutTest.php
@@ -25,35 +25,7 @@ class CheckoutTest extends DuskTestCase
                 ->assertAuthenticatedAs($user);
 
             $browser->visit(new Checkout)
-                ->assertTitle('Redirecting to PayHere...');
-
-            $browser->waitForText('Pay with', 10);
-
-            $browser->pause(1000);
-
-            $browser->click('@visa')
-                ->assertSee('Bank Card');
-
-            $browser->pause(1000);
-
-            $browser->withinFrame('@payment-frame', function (Browser $iframe) use ($user) {
-                $iframe->type('@card-holder-name', $user->name)
-                    ->assertInputValue('@card-holder-name', $user->name);
-
-                $iframe
-                    ->type('@card-no', '4916217501611292')
-                    ->assertInputValue('@card-no', '4916217501611292');
-
-                $iframe->type('@card-secure-id', '123')
-                    ->assertInputValue('@card-secure-id', '123');
-
-                $iframe->type('@card-expiry', now()->addYear()->format('m/y'))
-                    ->assertInputValue('@card-expiry', now()->addYear()->format('m/y'));
-
-                $iframe->pause(1000);
-
-                $iframe->press('@pay');
-            });
+                ->pay($user);
 
             $browser->waitForText('Payment Approved', 10);
         });
--- tests/Browser/Pages/Checkout.php
+++ tests/Browser/Pages/Checkout.php
@@ -2,13 +2,21 @@
 
 namespace Dasundev\PayHere\Tests\Browser\Pages;
 
+use Laravel\Dusk\Browser;
 use Laravel\Dusk\Page;
+use Workbench\App\Models\User;
 
 class Checkout extends Page
 {
     public function url(): string
     {
         return '/checkout';
+    }
+
+    public function assert(Browser $browser): void
+    {
+        $browser->assertRouteIs('checkout')
+            ->assertTitle('Redirecting to PayHere...');
     }
 
     public static function siteElements(): array
@@ -22,5 +30,36 @@ class Checkout extends Page
             '@card-expiry' => "input[name='cardExpiry']",
             '@pay' => "button[type='submit'].btn-primary",
         ];
+    }
+
+    public function pay(Browser $browser, User $user): void
+    {
+        $browser->waitForText('Pay with', 10);
+
+        $browser->pause(1000);
+
+        $browser->click('@visa')
+            ->assertSee('Bank Card');
+
+        $browser->pause(1000);
+
+        $browser->withinFrame('@payment-frame', function (Browser $iframe) use ($user) {
+            $iframe->type('@card-holder-name', $user->name)
+                ->assertInputValue('@card-holder-name', $user->name);
+
+            $iframe
+                ->type('@card-no', '4916217501611292')
+                ->assertInputValue('@card-no', '4916217501611292');
+
+            $iframe->type('@card-secure-id', '123')
+                ->assertInputValue('@card-secure-id', '123');
+
+            $iframe->type('@card-expiry', now()->addYear()->format('m/y'))
+                ->assertInputValue('@card-expiry', now()->addYear()->format('m/y'));
+
+            $iframe->pause(1000);
+
+            $iframe->press('@pay');
+        });
     }
 }
