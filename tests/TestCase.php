6704f6a85d8e19bfd02eea3ea5173774b81554fd
Dasun Tharanga <hello@dasun.dev>
2024-05-22 08:39:22 +0530

    test: get package providers in test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,18 @@
 
 namespace Dasundev\PayHere\Tests;
 
+use Dasundev\PayHere\PayHereServiceProvider;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use WithWorkbench;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            PayHereServiceProvider::class
+        ];
+    }
 }
