d1597e69cf4fe48d4fc927e1d8f073a5bfc1709c
Dasun Tharanga <hello@dasun.dev>
2024-05-23 15:22:27 +0530

    feat: update code

--- src/Concerns/CheckoutFormData.php
+++ src/Concerns/CheckoutFormData.php
@@ -78,9 +78,9 @@ trait CheckoutFormData
         return [
             'action' => $this->actionUrl(),
             'merchant_id' => config('payhere.merchant_id'),
+            'notify_url' => config('payhere.notify_url') ?? URL::signedRoute('payhere.webhook'),
             'return_url' => config('payhere.return_url') ?? URL::signedRoute('payhere.return'),
             'cancel_url' => config('payhere.cancel_url') ?? url('/'),
-            'notify_url' => config('payhere.notify_url') ?? URL::signedRoute('payhere.webhook'),
             'order_id' => $this->order->id,
             'items' => "Order #{$this->order->id}",
             'currency' => config('payhere.currency'),
