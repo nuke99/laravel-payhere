2c7eae7b220fd83a07cab1baa45e1058b7219c04
Dasun Tharanga <hello@dasun.dev>
2024-05-24 14:03:21 +0530

    feat: has install command

--- src/PayHereServiceProvider.php
+++ src/PayHereServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Dasundev\PayHere;
 
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -11,12 +12,26 @@ class PayHereServiceProvider extends PackageServiceProvider
     {
         $package
             ->name('payhere')
+            ->hasViews()
+            ->runsMigrations()
+            ->hasConfigFile()
+            ->hasAssets()
             ->hasRoutes(['web', 'api'])
             ->hasMigrations([
                 'create_payments_table',
             ])
-            ->hasViews()
-            ->runsMigrations()
-            ->hasConfigFile();
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishAssets()
+                    ->publishMigrations()
+                    ->askToRunMigrations()
+                    ->askToStarRepoOnGitHub('dasundev/laravel-payhere')
+                    ->endWith(function(InstallCommand $command) {
+                        $command->newLine();
+                        $command->info('Thank you so much for purchasing Laravel PayHere package!');
+                        $command->info("If you need any assistance, don't hesitate to reach out to me at hello@dasun.dev.");
+                    });
+            });
     }
 }
