a7997e1318fc92c2bd7b5aedd9fdc23ec9da1880
Dasun Tharanga <hello@dasun.dev>
2024-05-25 21:03:45 +0530

    test: charge type test cases

---
+++ tests/Unit/Rules/ChargeTypeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Dasundev\PayHere\Rules\ChargeType;
+
+test('valid charge type', function ($chargeType) {
+    $rule = new ChargeType();
+
+    $fail = fn (string $errorMessage) => $this->fail($errorMessage);
+
+    $rule->validate('type', $chargeType, $fail);
+
+    expect(true)->toBeTrue();
+})->with([
+    'PAYMENT',
+    'AUTHORIZE'
+]);
+
+test('invalid charge type', function ($chargeType) {
+    $rule = new ChargeType();
+
+    $fail = fn (string $errorMessage) => $this->fail($errorMessage);
+
+    $rule->validate('type', $chargeType, $fail);
+
+    expect(true)->toBeTrue();
+})->with([
+    'NOT EXISTS'
+])->fails();
