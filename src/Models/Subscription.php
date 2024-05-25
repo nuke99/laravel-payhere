ebaf2987b41fa0d22f17160fd30fa62a823e0e5c
Dasun Tharanga <hello@dasun.dev>
2024-05-25 18:00:22 +0530

    feat: add `HasTrial` concern

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -2,11 +2,11 @@
 
 namespace Dasundev\PayHere\Models;
 
+use Dasundev\PayHere\Concerns\HasTrial;
 use Dasundev\PayHere\Enums\SubscriptionStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Carbon;
 use Workbench\App\Models\Order;
 use Workbench\App\Models\User;
 use Workbench\Database\Factories\SubscriptionFactory;
@@ -14,20 +14,13 @@ use Workbench\Database\Factories\SubscriptionFactory;
 class Subscription extends Model
 {
     use HasFactory;
+    use HasTrial;
 
     protected $guarded = [];
 
     protected $casts = [
         'status' => SubscriptionStatus::class,
     ];
-
-    /**
-     * Determine if the subscription is within its trial period.
-     */
-    public function onTrial(): bool
-    {
-        return $this->trial_ends_at && $this->trial_ends_at->isFuture();
-    }
 
     public function user(): BelongsTo
     {
