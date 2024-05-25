8ba77aeaa69d6bdd7cb95d1a7c6fec90efd49fc3
Dasun Tharanga <hello@dasun.dev>
2024-05-25 21:27:33 +0530

    refactor: event name

--- src/Events/SubscriptionActivated.php
+++ src/Events/SubscriptionActivated.php
@@ -7,7 +7,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SubscriptionCreated
+class SubscriptionActivated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
