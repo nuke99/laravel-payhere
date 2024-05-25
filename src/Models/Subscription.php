80e291b7278a4eb0c0ec645b6fa8444a5cb68eda
Dasun Tharanga <hello@dasun.dev>
2024-05-25 15:22:58 +0530

    feat:  add `onTrial()`

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -5,6 +5,7 @@ namespace Dasundev\PayHere\Models;
 use Dasundev\PayHere\Enums\SubscriptionStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Workbench\Database\Factories\SubscriptionFactory;
 
 class Subscription extends Model
@@ -16,6 +17,14 @@ class Subscription extends Model
     protected $casts = [
         'status' => SubscriptionStatus::class,
     ];
+
+    /**
+     * Determine if the subscription is within its trial period.
+     */
+    public function onTrial(): bool
+    {
+        return $this->trial_ends_at && $this->trial_ends_at->isFuture();
+    }
 
     protected static function newFactory(): SubscriptionFactory
     {
