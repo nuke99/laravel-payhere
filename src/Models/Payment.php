d4ad37eda3dcbed1d22f1ebe8b7ccbfb89e268ba
Dasun Tharanga <hello@dasun.dev>
2024-05-25 17:06:16 +0530

    feat: update `Payment` model

--- src/Models/Payment.php
+++ src/Models/Payment.php
@@ -5,10 +5,16 @@ namespace Dasundev\PayHere\Models;
 use Dasundev\PayHere\Enums\MessageType;
 use Dasundev\PayHere\Enums\PaymentMethod;
 use Dasundev\PayHere\Enums\PaymentStatus;
+use Dasundev\PayHere\PayHere;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Workbench\Database\Factories\PaymentFactory;
 
 class Payment extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     protected $casts = [
@@ -16,4 +22,14 @@ class Payment extends Model
         'status_code' => PaymentStatus::class,
         'message_type' => MessageType::class,
     ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(PayHere::$orderModel);
+    }
+
+    protected static function newFactory(): PaymentFactory
+    {
+        return new PaymentFactory;
+    }
 }
