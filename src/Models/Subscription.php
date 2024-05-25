d7930a8062ff10bdd9c26dcc765b8a08d4c29004
Dasun Tharanga <hello@dasun.dev>
2024-05-25 17:41:31 +0530

    test: add subscription model tests

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -5,7 +5,10 @@ namespace Dasundev\PayHere\Models;
 use Dasundev\PayHere\Enums\SubscriptionStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
+use Workbench\App\Models\Order;
+use Workbench\App\Models\User;
 use Workbench\Database\Factories\SubscriptionFactory;
 
 class Subscription extends Model
@@ -24,6 +27,16 @@ class Subscription extends Model
     public function onTrial(): bool
     {
         return $this->trial_ends_at && $this->trial_ends_at->isFuture();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
     }
 
     protected static function newFactory(): SubscriptionFactory
---
+++ tests/Unit/Models/SubscriptionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Dasundev\PayHere\Models\Subscription;
+use Workbench\App\Models\Order;
+use Workbench\App\Models\User;
+
+test('to array', function () {
+    $payment = Subscription::factory()->create()->fresh();
+
+    expect(array_keys($payment->toArray()))->toBe([
+        'id',
+        'user_id',
+        'order_id',
+        'trial_ends_at',
+        'ends_at',
+        'status',
+        'created_at',
+        'updated_at',
+    ]);
+});
+
+test('relations', function () {
+    $subscription = Subscription::factory()->create();
+
+    expect($subscription->user)->toBeInstanceOf(User::class)
+        ->and($subscription->order)->toBeInstanceOf(Order::class);
+});
