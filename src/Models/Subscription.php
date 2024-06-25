f1d13d8d2cf98d2b487411ec941451db6b57e761
Dasun Tharanga <hello@dasun.dev>
2024-06-25 16:30:21 +0530

    fix: relationships

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -4,11 +4,10 @@ namespace Dasundev\PayHere\Models;
 
 use Dasundev\PayHere\Concerns\HasTrial;
 use Dasundev\PayHere\Enums\SubscriptionStatus;
+use Dasundev\PayHere\PayHere;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Workbench\App\Models\Order;
-use Workbench\App\Models\User;
 use Workbench\Database\Factories\SubscriptionFactory;
 
 class Subscription extends Model
@@ -24,12 +23,12 @@ class Subscription extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(PayHere::$customerModel);
     }
 
     public function order(): BelongsTo
     {
-        return $this->belongsTo(Order::class);
+        return $this->belongsTo(PayHere::$orderModel);
     }
 
     protected static function newFactory(): SubscriptionFactory
