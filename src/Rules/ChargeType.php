127b217e859036e9ced1ff9a6c3f069a43dac69d
Dasun Tharanga <hello@dasun.dev>
2024-06-25 23:40:41 +0530

    feat: update charge type rule

--- src/Rules/ChargeType.php
+++ src/Rules/ChargeType.php
@@ -4,14 +4,15 @@ namespace Dasundev\PayHere\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Dasundev\PayHere\Enums\ChargeType as ChargeTypeEnum;
 
 class ChargeType implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (! in_array($value, [
-            \Dasundev\PayHere\Enums\ChargeType::PAYMENT->name,
-            \Dasundev\PayHere\Enums\ChargeType::AUTHORIZE->name,
+            ChargeTypeEnum::PAYMENT->name,
+            ChargeTypeEnum::AUTHORIZE->name,
         ], true)) {
             $fail('The :attribute must be either PAYMENT or AUTHORIZE.');
         }
