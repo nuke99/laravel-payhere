aac55e212c6af149f36e50aa42ebdf72f0c2e468
Dasun Tharanga <hello@dasun.dev>
2024-06-28 21:48:15 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'payhere', 'as' => 'payhere.'], function () {
     Route::get('/return', [PayHereController::class, 'handleReturn'])->name('return');
-    Route::post('/webhook', [WebhookController::class, 'handleWebhook'])->middleware('signed')->name('webhook');
+    Route::post('/webhook', [WebhookController::class, 'handleWebhook'])->name('webhook');
 });
